ab98a1d3a0596b7626c73472018ed05035c0c8b4
can <cansarigol@derinbilgi.com.tr>
2018-02-16 16:14:32 +0300

    .

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python::3',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
